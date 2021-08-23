5c61c5db0141147d2e8c7df35e9084b1f26f5f68
Yan Sern <yan.sern@automattic.com>
2021-08-23 15:46:58 +0200

    Fix api is undefined.

--- src/customize-guide.js
+++ src/customize-guide.js
@@ -1,10 +1,12 @@
 import $ from 'jquery';
+import getAPI from './helpers/api';
 
 import addGuide from './modules/guide';
 import getOpts from './helpers/options';
 import debugFactory from 'debug';
 
 const debug = debugFactory( 'customize-guide' );
+const api = getAPI();
 
 api.bind( 'ready', () => {
 	debug( 'customize guide is ready' );
